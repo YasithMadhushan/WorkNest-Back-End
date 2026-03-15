0eb7ba17f3769f9748ea8c5049ab512075433b27
YasithMadhushan <yasithmadhushan416@gmail.com>
2026-03-15 20:25:14 +0530

    Initial project commit

---
+++ freelancingbackendservice/src/main/java/com/example/freelancingbackendservice/FreelancingbackendserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.freelancingbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancingbackendserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancingbackendserviceApplication.class, args);
+	}
+
+}
---
+++ freelancingbackendservice/src/test/java/com/example/freelancingbackendservice/FreelancingbackendserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelancingbackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancingbackendserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
